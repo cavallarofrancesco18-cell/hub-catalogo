265507d9817425ccddf9d7af0e7197cf25a06aea
Francesco Cavallaro <cavallaro.francesco18@gmail.com>
2026-03-03 21:44:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3074982188-44660",
+  "appId": "1:390608264660:web:121339e49071006e16ca41",
+  "apiKey": "AIzaSyADIr4oyzev7kurADsA4LTrYB23KSvDoiQ",
+  "authDomain": "studio-3074982188-44660.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "390608264660"
+};
